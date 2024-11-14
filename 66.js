a072cd0c4aac26f3022e1cde244fba0f3488929b
Jeetaksh25 <manojjeetakshluhaniwal25@gmail.com>
2024-11-14 09:10:12 +0530

    14/11/24

--- 66.js
+++ 66.js
@@ -18,8 +18,75 @@ class Honda extends Car{
     }
 };
 
-var obj= new Honda();
-obj.setEngineModel("3.5L Twin-Turbo V6");
-obj.setCarModel("Acura NSX");
-obj.getEngineModel();
-obj.getCarModel()
+class Polygon{
+    constructor(){
+        this.sideNo=0;
+        this.choice=0;
+    }
+    
+    getChoice(){
+        console.log("1. Rectangle\n2. Pentagon");
+        this.choice=parseInt(prompt("Enter your choice: "));
+        if(this.choice===1){
+            this.sideNo=4;
+            return new Rectangle();
+        }
+        else if(this.choice===2){
+            this.sideNo=5;
+            return new Pentagon();
+        }
+        else{
+            console.log("Invalid choice");
+            return null;
+        }
+    }
+};
+
+class Rectangle extends Polygon{
+    constructor(){
+        super();
+        this.sideNo=4;
+        this.sides=[];
+    }
+
+    inputSides(){
+        for (let i=0;i<this.sideNo;i++){
+            this.sides.push(parseInt(prompt(`Enter side ${i+1}: `)));
+        }
+        return this.sides;
+    }
+
+    displaySides(){
+        for (let i=0;i<this.sideNo;i++){
+            console.log(`Side ${i+1}: ${this.sides[i]}`)
+        }
+    }
+};
+
+class Pentagon extends Polygon{
+    constructor(){
+        super();
+        this.sideNo=5;
+        this.sides=[];
+    }
+
+    inputSides(){
+        for (let i=0;i<this.sideNo;i++){
+            this.sides.push(parseInt(prompt(`Enter side ${i+1}: `)));
+        }
+        return this.sides;
+    }
+
+    displaySides(){
+        for (let i=0;i<this.sideNo;i++){
+            console.log(`Side ${i+1}: ${this.sides[i]}`)
+        }
+    }
+};
+
+let obj=new Pentagon();
+choice = obj.getChoice();
+if (choice!=null){
+    choice.inputSides();
+    choice.displaySides();
+}
